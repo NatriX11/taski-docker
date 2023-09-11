2df5442564f5815afe90ae734af5de273adc2f71
NatriX11 <122643107+NatriX11@users.noreply.github.com>
2023-09-11 23:09:20 +0300

    Add workflow with PEP8 linting3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -113,7 +113,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
